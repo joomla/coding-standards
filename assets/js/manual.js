8eb859752eebba07d1a1967eb18ef495f723464b
Tom Hutchison <Hutchy68@gmail.com>
2013-12-18 22:30:15 -0500

    Messing around with JS on page

--- assets/js/manual.js
+++ assets/js/manual.js
@@ -1,3 +1,10 @@
+window.onload=function(){
+	var elem = document.createElement("img");
+	elem.setAttribute("src", "https://github.com/Hutchy68/coding-standards/blob/gh-pages/manual/en-US/coding-standards/joomla_coding_standards_manual.png?raw=true");
+	elem.setAttribute("alt", "Manual Graphic");
+	document.getElementById("manual-image").appendChild("elem");
+	}
+
 var populateWindow = function(link) {
 	var markdownRequest = new Request({
 		"url": here + 'manual/en-US/' + link,
@@ -64,12 +71,6 @@ window.addEvent('domready', function() {
 	else
 	{
 		var currentDoc = "coding-standards/introduction.md";
-		window.onload=function(){
-			var elem = document.createElement("img");
-			elem.setAttribute("src", "https://github.com/Hutchy68/coding-standards/blob/gh-pages/manual/en-US/coding-standards/joomla_coding_standards_manual.png?raw=true");
-			elem.setAttribute("alt", "Manual Graphic");
-			document.getElementById("manual-image").appendChild("elem");
-		}
 	}
 
 	marked.setOptions({
