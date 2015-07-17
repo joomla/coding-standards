87a9c5ccaa15eabb718e110dcd3e228c141dfc6f
Michael Babker <mbabker@flbab.com>
2015-07-17 10:52:20 -0400

    Get the sniffs up to standard

--- Sniffs/ControlStructures/ControlSignatureSniff.php
+++ Sniffs/ControlStructures/ControlSignatureSniff.php
@@ -1,62 +1,49 @@
 <?php
 /**
- * Verifies that control statements conform to their coding standards.
+ * Joomla! Coding Standard
  *
- * PHP version 5
- *
- * @category  PHP
- * @package   PHP_CodeSniffer
- * @author    Greg Sherwood <gsherwood@squiz.net>
- * @author    Marc McIntyre <mmcintyre@squiz.net>
- * @copyright 2006-2014 Squiz Pty Ltd (ABN 77 084 670 600)
- * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
- * @link      http://pear.php.net/package/PHP_CodeSniffer
+ * @copyright  Copyright (C) Open Source Matters, Inc. All rights reserved.
+ * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
  */
-if (class_exists('PHP_CodeSniffer_Standards_AbstractPatternSniff', true) === false) {
-    throw new PHP_CodeSniffer_Exception('Class PHP_CodeSniffer_Standards_AbstractPatternSniff not found');
+
+if (class_exists('PHP_CodeSniffer_Standards_AbstractPatternSniff', true) === false)
+{
+	throw new PHP_CodeSniffer_Exception('Class PHP_CodeSniffer_Standards_AbstractPatternSniff not found');
 }
+
 /**
  * Verifies that control statements conform to their coding standards.
- *
- * @category  PHP
- * @package   PHP_CodeSniffer
- * @author    Greg Sherwood <gsherwood@squiz.net>
- * @author    Marc McIntyre <mmcintyre@squiz.net>
- * @copyright 2006-2014 Squiz Pty Ltd (ABN 77 084 670 600)
- * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
- * @version   Release: @package_version@
- * @link      http://pear.php.net/package/PHP_CodeSniffer
  */
 class Joomla_Sniffs_ControlStructures_ControlSignatureSniff extends PHP_CodeSniffer_Standards_AbstractPatternSniff
 {
-    /**
-     * If true, comments will be ignored if they are found in the code.
-     *
-     * @var boolean
-     */
-    public $ignoreComments = true;
+	/**
+	 * If true, comments will be ignored if they are found in the code.
+	 *
+	 * @var  boolean
+	 */
+	public $ignoreComments = true;
 
-    /**
-     * A list of tokenizers this sniff supports.
-     *
-     * @var array
-     */
-    public $supportedTokenizers = array(
-                                   'PHP',
-                                   'JS',
-                                  );
+	/**
+	 * A list of tokenizers this sniff supports.
+	 *
+	 * @var  array
+	 */
+	public $supportedTokenizers = array(
+		'PHP',
+		'JS',
+	);
 
-    /**
-     * Returns the patterns that this test wishes to verify.
-     *
-     * @return string[]
-     */
-    protected function getPatterns()
-    {
+	/**
+	 * Returns the patterns that this test wishes to verify.
+	 *
+	 * @return  string[]
+	 */
+	protected function getPatterns()
+	{
 		return array(
-			'if (...)EOL',
-			'}EOL...elseif (...)EOL',
-			'}EOL...elseEOL',
+			'if (...)EOL...{EOL',
+			'}EOL...elseif (...)EOL...{EOL',
+			'}EOL...elseEOL...{EOL',
 			'tryEOL...{EOL...}EOL',
 			'catch (...)EOL...{EOL',
 			'doEOL...{...}EOL',
@@ -65,6 +52,5 @@ class Joomla_Sniffs_ControlStructures_ControlSignatureSniff extends PHP_CodeSnif
 			'foreach (...)EOL...{EOL',
 			'switch (...)EOL...{EOL',
 		);
-	}//end getPatterns()
-}//end class
-?>
+	}
+}
