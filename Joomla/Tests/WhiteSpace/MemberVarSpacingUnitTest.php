6efa9f72ebf75b17320cf076f4ab455047a36087
Sven Versteegen <redeye.vess@gmail.com>
2015-09-04 09:37:26 +0200

    custom sniff for MemberVarSpacingSniff

---
+++ Joomla/Tests/WhiteSpace/MemberVarSpacingUnitTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Joomla! Coding Standard
+ *
+ * @copyright  Copyright (C) 2015 Open Source Matters, Inc. All rights reserved.
+ * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
+ */
+
+/**
+ * InstantiateNewClassesUnitTest
+ */
+class Joomla_Tests_WhiteSpace_MemberVarSpacingUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {
+        return [
+                5  => 1,
+                8  => 1,
+                23 => 1,
+                28 => 1,
+                42 => 1,
+                45 => 1,
+               ];
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getWarningList()
+    {
+        return [];
+    }
+}
