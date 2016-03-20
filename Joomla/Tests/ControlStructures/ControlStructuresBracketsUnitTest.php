87b8b978e4b0d10ec5df2b4cad441dd8a4bd1d42
photodude <photodude@users.noreply.github.com>
2016-03-20 13:05:11 -0600

    Add `@package` tag

--- Joomla/Tests/ControlStructures/ControlStructuresBracketsUnitTest.php
+++ Joomla/Tests/ControlStructures/ControlStructuresBracketsUnitTest.php
@@ -9,7 +9,8 @@
 /**
  * ControlStructuresBracketsUnitTest
  *
- * @since   1.0
+ * @package   Joomla.CodingStandard
+ * @since     1.0
  */
 class Joomla_Tests_ControlStructures_ControlStructuresBracketsUnitTest extends AbstractSniffUnitTest
 {
