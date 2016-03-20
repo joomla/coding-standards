aee66cdc3c26e556bf468004735e5f529c423f79
photodude <photodude@users.noreply.github.com>
2016-03-20 13:24:11 -0600

    Add `@package` tag

--- Joomla/Tests/Operators/ValidLogicalOperatorsUnitTest.php
+++ Joomla/Tests/Operators/ValidLogicalOperatorsUnitTest.php
@@ -9,7 +9,8 @@
 /**
  * ValidLogicalOperatorsUnitTest
  *
- * @since   1.0
+ * @package   Joomla.CodingStandard
+ * @since     1.0
  */
 class Joomla_Tests_Operators_ValidLogicalOperatorsUnitTest extends AbstractSniffUnitTest
 {
