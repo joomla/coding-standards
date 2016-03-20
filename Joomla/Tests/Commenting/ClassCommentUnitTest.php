c69ae690ad6b2a48e6828cb7cc7cf74a7a1b507e
photodude <photodude@users.noreply.github.com>
2016-03-20 12:22:11 -0600

    Should error on line 4 with 2 errors
    Shange the comment breaks to simplify reading of the Key and Value meanings

--- Joomla/Tests/Commenting/ClassCommentUnitTest.php
+++ Joomla/Tests/Commenting/ClassCommentUnitTest.php
@@ -16,8 +16,8 @@ class Joomla_Tests_Commenting_ClassCommentUnitTest extends AbstractSniffUnitTest
 	/**
 	 * Returns the lines where errors should occur.
 	 *
-	 * The key of the array should represent the line number and the value
-	 * should represent the number of errors that should occur on that line.
+	 * The key of the array should represent the line number and 
+	 * the value should represent the number of errors that should occur on that line.
 	 *
 	 * @return array<int, int>
 	 */
@@ -29,8 +29,8 @@ class Joomla_Tests_Commenting_ClassCommentUnitTest extends AbstractSniffUnitTest
 	/**
 	 * Returns the lines where warnings should occur.
 	 *
-	 * The key of the array should represent the line number and the value
-	 * should represent the number of warnings that should occur on that line.
+	 * The key of the array should represent the line number and
+	 * the value should represent the number of warnings that should occur on that line.
 	 *
 	 * @return array<int, int>
 	 */
