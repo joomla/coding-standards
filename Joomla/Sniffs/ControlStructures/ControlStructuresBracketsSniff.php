0d43f65929d3eab833d22894f215caadc4c12811
Walt Sorensen <photodude@users.noreply.github.com>
2017-02-13 13:50:51 -0700

    too much code commented out

--- Joomla/Sniffs/ControlStructures/ControlStructuresBracketsSniff.php
+++ Joomla/Sniffs/ControlStructures/ControlStructuresBracketsSniff.php
@@ -154,9 +154,9 @@ class Joomla_Sniffs_ControlStructures_ControlStructuresBracketsSniff implements 
 				$spaces     = strlen($blankSpace);
 			}
 
+			$expected = ($tokens[$stackPtr]['level'] * ($this->indent));
 			// We need to divide by 4 here since there is a space vs tab intent in the check vs token
 			// $spaces = $spaces / 4;
-			//$expected = ($tokens[$stackPtr]['level'] * ($this->indent));
 			$expected   = floor($expected / $this->indent);
 			$spaces     = floor($spaces / $this->indent);
 
