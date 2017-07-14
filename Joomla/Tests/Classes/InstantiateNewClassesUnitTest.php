04dbac90dbfb596fb78b2ad2ae23f1317b0b6758
Arlen P Walker <arlen.walker@pkware.com>
2017-07-14 15:02:27 -0500

    Namespaced the unit tests
    Now to get the classes to autoload

--- Joomla/Tests/Classes/InstantiateNewClassesUnitTest.php
+++ Joomla/Tests/Classes/InstantiateNewClassesUnitTest.php
@@ -6,13 +6,15 @@
  * @copyright  Copyright (C) 2015 Open Source Matters, Inc. All rights reserved.
  * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
  */
+namespace Joomla\Tests\Classes;
 
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
 /**
  * Ensures that new classes are instantiated without brackets if they do not have any parameters.
  *
  * @since     1.0
  */
-class Joomla_Tests_Classes_InstantiateNewClassesUnitTest extends AbstractSniffUnitTest
+class InstantiateNewClassesUnitTest extends AbstractSniffUnitTest
 {
 	/**
 	 * Returns the lines where errors should occur.
--- Joomla/Tests/WhiteSpace/MemberVarSpacingUnitTest.php
+++ Joomla/Tests/WhiteSpace/MemberVarSpacingUnitTest.php
@@ -6,13 +6,15 @@
  * @copyright  Copyright (C) 2015 Open Source Matters, Inc. All rights reserved.
  * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
  */
+namespace Joomla\Tests\WhiteSpace;
 
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
 /**
  * InstantiateNewClassesUnitTest
  *
  * @since     1.0
  */
-class Joomla_Tests_WhiteSpace_MemberVarSpacingUnitTest extends AbstractSniffUnitTest
+class MemberVarSpacingUnitTest extends AbstractSniffUnitTest
 {
 	/**
 	 * Returns the lines where errors should occur.
