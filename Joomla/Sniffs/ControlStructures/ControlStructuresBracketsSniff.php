21308113febaa6debdc481a9bb7d35668d394963
Walt Sorensen <photodude@users.noreply.github.com>
2018-01-07 18:29:47 -0700

    Fix indent within Anonymous classes and functions

--- Joomla/Sniffs/ControlStructures/ControlStructuresBracketsSniff.php
+++ Joomla/Sniffs/ControlStructures/ControlStructuresBracketsSniff.php
@@ -173,7 +173,16 @@ class Joomla_Sniffs_ControlStructures_ControlStructuresBracketsSniff implements 
 				}
 			}
 
-			$expected = ($tokens[$stackPtr]['level'] * ($this->indent));
+			// Anonymous classes and functions set the indent at one plus their own indent level.
+			if ($phpcsFile->hasCondition($stackPtr, T_CLOSURE) === true
+				|| $phpcsFile->hasCondition($stackPtr, T_ANON_CLASS) === true)
+			{
+				$expected = ($tokens[$stackPtr]['level'] + 1) * $this->indent;
+			}
+			else
+			{
+				$expected = $tokens[$stackPtr]['level'] * $this->indent;
+			}
 
 			// We need to divide by 4 here since there is a space vs tab intent in the check vs token
 			$expected /= $this->indent;
